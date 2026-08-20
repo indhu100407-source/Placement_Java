e43df22f8bfd30450b78e1a7e2c4f2ef48727b3b
indhu <indhu100407@gmail.com>
2026-08-20 15:32:31 +0530

    LastDigit

---
+++ LastDigit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LastDigit {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int lastDigit = num % 10;
+
+        System.out.println("Last digit is: " + lastDigit);
+
+        sc.close();
+    }
+}
