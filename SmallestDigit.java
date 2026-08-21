6dbf7734ea2cecac71ddd261e8a1f7aee112973d
indhu <indhu100407@gmail.com>
2026-08-21 08:54:58 +0530

    SmallestDigit

---
+++ SmallestDigit.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SmallestDigit {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        num = Math.abs(num);
+
+        int smallest = 9;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit < smallest) {
+                smallest = digit;
+            }
+
+            num = num / 10;
+        }
+
+        System.out.println("Smallest digit is: " + smallest);
+
+        sc.close();
+    }
+}
