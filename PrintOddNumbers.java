98e51bf896f221d2ee77c43ed303d2f173f20e01
indhu <indhu100407@gmail.com>
2026-08-21 20:59:37 +0530

    PrintOddNumbers

---
+++ PrintOddNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrintOddNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Odd numbers from 1 to " + n + ":");
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
