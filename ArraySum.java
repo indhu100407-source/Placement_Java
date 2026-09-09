061ab0a35a99d24c27c93de9e83e4504ad3eed11
indhu <indhu100407@gmail.com>
2026-09-09 10:50:06 +0530

    ArraySum

---
+++ ArraySum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArraySum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
+
